ac506cb8ffa2ec1fb8c48ba4d84caf15c719d990
ShahidHaris <alharithshaheed@yahoo.com>
2022-10-21 20:10:30 +0100

    set up possible_combination to get the different combination of the word

--- main.rb
+++ main.rb
@@ -1,3 +1,18 @@
 def substrings(word, collection)
-    
+
+    def possible_combinations(str)
+        combination = []
+
+        string = str.downcase.gsub(/[^a-z0-9\s]/i, '').split.join
+        i = 0
+        while i < string.length
+            j = i + 1
+            while j < (string.length + 1)
+                combination.push(string[i...j])
+                j += 1
+            end
+            i += 1
+        end
+        combination
+    end
 end
